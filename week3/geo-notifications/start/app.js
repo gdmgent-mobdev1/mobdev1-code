5baa711ce2d70a11b48936552b2b9c1aaf59aa7b
timdpaep <tim.depaepe@gmail.com>
2020-10-07 13:40:48 +0200

    added startcode for week3

---
+++ week3/geo-notifications/start/app.js
@@ -0,0 +1,28 @@
+/**
+ * Geolocation & Notifications API
+ */
+
+// ----
+// Notifications
+// ----
+
+// TODO: Write function that checks Notification API permissions
+
+// TODO: Write a function that will show a notification
+
+// get the notification button
+const notificationButton = document.getElementById('showNotification');
+notificationButton.addEventListener('click', () => {
+  // TODO: Show notification right here!
+});
+
+// ----
+// Geolocation
+// ----
+
+// TODO: Write function that will catch error codes
+// TIP: check documentation of MDN to see what kind of error codes there are
+
+// TODO: Write a function that will get your location
+
+// TODO: get location and append to the DOM
---
+++ week3/pokeapi/start/app.js
@@ -0,0 +1,16 @@
+/**
+ * Get Some Pokemon!
+ */
+
+// TODO: Create a function that will fetch 20 pokemon via the endpoint https://pokeapi.co/api/v2/pokemon/
+
+// TODO: Create a function that will store pokemon in the localstorage.
+// IMPORTANT: NEVER overwrite an existing pokomen array in storage
+
+// TODO: Create a function that can change the name of an existing pokemon in the localStorage
+// by giving the pokemonName and the newName as a parameter
+
+// You should end up with this chain:
+// getPokemon()
+//   .then(storePokemon)
+//   .then(() => changePokemon('bulbasaur', 'CoolPokemon!'));
