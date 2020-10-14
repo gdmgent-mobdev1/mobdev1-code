231749551f61af1cb787c079e8736b67790eef75
timdpaep <tim.depaepe@gmail.com>
2020-10-15 00:57:22 +0200

    added week 4

---
+++ week4/history/start/app.js
@@ -0,0 +1,8 @@
+/**
+ * The History API
+ */
+
+// TODO: Let the navigation work and show the contents.
+// e.g. link with contentid=1 shows main>div.content-1, etc..
+// TODO: Keep clicked links in the browser history so we can navigate
+// back and forth
---
+++ week4/webaudio/start/app.js
@@ -0,0 +1,10 @@
+/**
+ * The Boombox App
+ */
+
+// TODO: Create a boombox that'll play outfoxing.mp3 from the assets folder
+// TODO: Init the app with an audiocontext, volumecontrol and pannercontrol
+// TODO: Let the boombox play by clicking on the play button
+// TODO: Let the boombox pause by clicking the play button again
+// TODO: Let the boombox stop by clicking the power button
+// TODO: When the audio file ends, set the pause icon back to play icon
